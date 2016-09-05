7153183888dbfb5a1463ba4a78094f448efe1e87
Dmytro Maherovskyi <xxmdvxx@gmail.com>
2016-09-05 18:40:07 +0300

    Model created

---
+++ FileSystemBrowser/FileSystemBrowser/Models/FileInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FileSystemBrowser.Models
+{
+    public class FileInfo
+    {
+        // File counters.
+        public int FileUnder10MbCounter { get; set; }
+        public int File10To50MbCounter { get; set; }
+        public int FileOver100MbCounter { get; set; }
+
+        // Files name container.
+        public List<string> FileDirectory { get; set; }
+    }
+}
